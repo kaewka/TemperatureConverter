eeb2acdeba5cf2c1cf253ebf2529bd5cdcf01b9e
Thanawat Kaewka <thanawat@yahoo-inc.com>
2014-01-11 17:07:20 -0800

    Initial Commit

---
+++ Temperature Converter/Temperature Converter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Temperature Converter
+//
+//  Created by Thanawat Kaewka on 1/11/14.
+//  Copyright (c) 2014 thanawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Temperature Converter/Temperature Converter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Temperature Converter
+//
+//  Created by Thanawat Kaewka on 1/11/14.
+//  Copyright (c) 2014 thanawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
